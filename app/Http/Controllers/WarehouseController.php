f2dffcb2cc72d37c4064fd4fbc42aad0b94a1917
dariusr <darius.r111@gmail.com>
2022-11-30 10:35:39 +0200

    First commit

---
+++ app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Warehouse;
+use Illuminate\Http\Request;
+
+class WarehouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('warehouses.index', ['warehouses'=>Warehouse::all()]);
+        //pasiduodam visus sandėlius į blade
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('warehouses.edit');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Warehouse::create($request->all());
+        return redirect()->route('warehouse.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(warehouse $warehouse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(warehouse $warehouse)
+    {
+        return view('warehouses.edit',['warehouse'=>$warehouse]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, warehouse $warehouse)
+    {
+        $warehouse->fill($request->all());
+        $warehouse->save();
+        return redirect()->route('warehouse.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\warehouse  $warehouse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(warehouse $warehouse)
+    {
+        $warehouse->delete();
+        return redirect()->route('warehouse.index');
+    }
+}
